f512981ab7fe675b430c2bf69f81ed5f47fbf7de
ehsia-hsia <erica.hsia1@gmail.com>
2021-02-16 16:54:52 -0500

    Add Second Image
    Second flip image added with new images. I would like to find a way to streamline this without repeating basically the same function for all images. So I'd like to create one function that changes the background depending on which "book button" is clicked

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-let book = document.querySelector('.flip');
+let book = document.querySelector('.flip1');
 book.addEventListener("click", myFunction2);
 
 function myFunction2() {
-    let bookButton = document.getElementById("round");
+    let bookButton = document.getElementById("nalo");
     if (bookButton.style.backgroundImage.match("MidnightRobber-crop.jpg")) {
         bookButton.style.backgroundImage = "url(https://images-na.ssl-images-amazon.com/images/I/61h6zjbZxKL.jpg)";
         bookButton.style.backgroundSize = "cover";
@@ -12,4 +12,18 @@ function myFunction2() {
     }
 }
 
+let book1 = document.querySelector('.flip2');
+book1.addEventListener("click", myFunction3);
 
+function myFunction3() {
+    let bookButton1 = document.getElementById("binti");
+    if (bookButton1.style.backgroundImage.match("1537451432l/37877850.jpg")) {
+        bookButton1.style.backgroundImage = "url(https://images-na.ssl-images-amazon.com/images/I/81IICHFn4LL.jpg)";
+        bookButton1.style.backgroundSize = "cover";
+        bookButton1.style.backgroundPosition = "center top";
+    } else {
+        bookButton1.style.backgroundImage = "url(https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1537451432l/37877850.jpg)";
+        bookButton1.style.backgroundSize = "contain";
+        bookButton1.style.backgroundPosition = "center";
+    }
+}
