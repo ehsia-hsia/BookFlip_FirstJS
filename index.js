9ba60169a6e28b770f3f6e3142fbe9b77cda8a71
ehsia-hsia <erica.hsia1@gmail.com>
2021-02-18 11:31:03 -0500

    Single Function
    This is a set of images that change background image on click. This is currently from a single function with each div id in an array and each image url in a separate array. a loop iterates through each array and then passes it through a function that changes the image background. only the array items need to be changed or added, not the function. However, I cannot get the images to return the original background on a second click, will work on this.

--- index.js
+++ index.js
@@ -30,3 +30,32 @@ function myFunction3() {
         text.innerHTML = "red";
     }
 }
+
+
+// let item = document.getElementsByClassName("image");
+let blue = document.getElementById("blue");
+let red = document.getElementById("red");
+let green = document.getElementById("green");
+
+let curItem = [blue, red, green];
+
+let backG = ["url(https://i0.wp.com/www.tor.com/wp-content/uploads/2018/05/MidnightRobber-crop.jpg?fit=740%2C+9999&crop=0%2C0%2C100%2C385px&ssl=1)", "url(https://upload.wikimedia.org/wikipedia/en/7/77/Midnight_Robber.jpg)", "url(https://images-na.ssl-images-amazon.com/images/I/61h6zjbZxKL.jpg)"];
+
+for (let i = 0; i < curItem.length; i++) {
+    curItem[i].addEventListener("click", myFunction);
+    function myFunction() {
+
+        curItem[i].style.backgroundImage = (backG[i]);
+        // curItem[i].style.backgroundImage = (backG[i])
+
+
+
+
+        // for (let i = 0; i < backG.length; i++){}
+        //  curItem[i].style.backgroundImage = (backG[i])
+
+
+
+    }//end function
+
+}//end first loop
