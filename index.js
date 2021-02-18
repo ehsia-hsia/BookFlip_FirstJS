027f23cda3baaadd903307e6ba13e300e1c2630a
ehsia-hsia <erica.hsia1@gmail.com>
2021-02-18 12:24:03 -0500

    Conditional Tests
    working on creating a conditional to switch image back to original,

--- index.js
+++ index.js
@@ -33,7 +33,6 @@ function myFunction3() {
 }
 
 
-// let item = document.getElementsByClassName("image");
 let blue = document.getElementById("blue");
 let red = document.getElementById("red");
 let green = document.getElementById("green");
@@ -42,18 +41,18 @@ let curItem = [blue, red, green];
 
 let backG = ["url(https://i0.wp.com/www.tor.com/wp-content/uploads/2018/05/MidnightRobber-crop.jpg?fit=740%2C+9999&crop=0%2C0%2C100%2C385px&ssl=1)", "url(https://upload.wikimedia.org/wikipedia/en/7/77/Midnight_Robber.jpg)", "url(https://images-na.ssl-images-amazon.com/images/I/61h6zjbZxKL.jpg)"];
 
-for (let i = 0; i < curItem.length; i++) {
+for (let i = 0; i < curItem.length; i++) { // assign i 
     curItem[i].addEventListener("click", myFunction);
+
     function myFunction() {
 
-        curItem[i].style.backgroundImage = (backG[i]);
-        // curItem[i].style.backgroundImage = (backG[i])
+        if (curItem[i].style.backgroundColor.match("white")) {
+            curItem[i].style.backgroundImage = (backG[i]);
+        } else {
+            curItem[i].style.backgroundImage = (backG[i]);
+        }
 
-
-
-
-        // for (let i = 0; i < backG.length; i++){}
-        //  curItem[i].style.backgroundImage = (backG[i])
+        // curItem[i].style.backgroundImage = (backG[i]);
 
 
 
